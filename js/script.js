5023a057c21af971e3172be7cf058e9eb00eb9f9
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-03-11 12:51:40 +0700

    add event upload file to display list-file has uploaded

--- js/script.js
+++ js/script.js
@@ -7,8 +7,31 @@ document.addEventListener("DOMContentLoaded", () => {
   inputFile.addEventListener("change", () => {
     fileList.innerHTML = "";
     numOfFiles.textContent = `${inputFile.files.length} Files selected`;
-  });
 
-  const fileName = document.getElementById("file-name");
-  const fileSize = document.getElementById("file-size");
+    for (file of inputFile.files) {
+      let reader = new FileReader();
+      let fileName = file.name;
+      let fileSize = (file.size / 1024).toFixed(0);
+      let listItem = document.createElement("li");
+
+      listItem.innerHTML = `
+      <span class="detail-file">
+        <img src="./img/File.png" alt="File Icon" />
+        <p class="file-name">${fileName}</p>
+      </span>
+      <p class="file-size">${fileSize} KB</p>`;
+
+      if (fileSize >= 1024) {
+        fileSize = (file.size / 1024).toFixed(1);
+        listItem.innerHTML = `
+        <span class="detail-file">
+          <img src="./img/File.png" alt="File Icon" />
+          <p class="file-name">${fileName}</p>
+        </span>
+        <p class="file-size">${fileSize} MB</p>`;
+      }
+
+      fileList.appendChild(listItem);
+    }
+  });
 });
